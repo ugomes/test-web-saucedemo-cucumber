d95a56acf1a30ff4deff924f9ea99e9452cdecb6
Uelton Gomes <102355027+ugomes@users.noreply.github.com>
2025-10-28 22:09:13 -0300

    chore: ajustes finais para rodar testes em modo headless e padronização CommonJS

--- features/hooks.cjs
+++ features/hooks.cjs
@@ -3,7 +3,7 @@ const { chromium } = require('playwright');
 const LoginPage = require('./support/pages/LoginPage.cjs');
 
 Before(async function () {
-    this.browser = await chromium.launch({ headless: false });
+    this.browser = await chromium.launch({ headless: true });
     this.page = await this.browser.newPage();
     this.loginPage = new LoginPage(this.page); 
     await this.loginPage.goto();
